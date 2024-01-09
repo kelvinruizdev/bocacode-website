d959169a3c5fe8a960cce968c14f4e74dc134570
Kelvin <kelvinruiz.developer@gmail.com>
2024-01-09 18:30:14 -0400

    correcion para pasar deployment

--- app/components/Testimonials/page.js
+++ app/components/Testimonials/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Link from "next/link";
 import React, { useState, useRef } from "react";
 import TitleContainer from "../TitleContainer/page";
--- app/components/VideoModal/page.js
+++ app/components/VideoModal/page.js
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect, useRef } from "react";
 import ReactPlayer from 'react-player/youtube'
 import '../../styles/card.css'
