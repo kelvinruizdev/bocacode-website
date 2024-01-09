a4f127d8082d5b65680dc3f0c5c41004351e8b27
Kelvin <kelvinruiz.developer@gmail.com>
2024-01-09 02:31:19 -0400

    software-engineering responsive

--- app/components/UpcomingDates/page.js
+++ app/components/UpcomingDates/page.js
@@ -5,7 +5,7 @@ const UpcomingDates = ({ data }) => {
 
   return (<>
 
-    <div className="py-50">
+    <div className="py-50 p-sm-20">
       <TitleContainer
         data={data?.title_container}
         classContainer="text-center w-50  m-auto gap-16"
@@ -13,32 +13,34 @@ const UpcomingDates = ({ data }) => {
         classParagraph="fs-16 fw-400 lh-28"
       />
 
-      <div className="py-50">
-        <table className="maxwidth-1366 m-auto box-shadow-v1">
-          <tr className="border-v2">
+      <div className="py-50 overflow-x-auto">
+        <table className="maxwidth-1366 m-auto box-shadow-v2 rounded-8 ">
+          <thead className="border-v2">
             {data?.fields &&
               data?.fields.map(field =>
-                <td className=" text-center p-16 fs-14 fw-400 lh-20">{field}</td>
+                <th className=" text-center p-16 fs-14 fw-400 lh-20">{field}</th>
               )}
-          </tr>
-          <tr>
-            {data?.date_info &&
-              data?.date_info.map(info => {
-                return (<>
-                  <td className="p-12 row">
+          </thead>
+          
+          <tbody > 
+            <tr > 
+              {data?.date_info &&
+                data?.date_info.map(info => {
+                  return (<> 
+                    <td className="p-12 row column-tablet ">
+                      <div className="p-12 fs-14 fw-600 lh-20"> {info.course.title}</div>
+                      <div className="p-12">
+                        <span className="bg-light-gray rounded-16 p-4-12 fs-12 fw-600 lh-16 color-lightgray">{info.course.modality}</span>
+                      </div>
+                    </td>
+                    <td className="p-12 fs-14 fw-400 lh-20 text-center color-lightgray">{info.start_date.date}</td>
+                    <td className="p-12 fs-14 fw-400 lh-20 text-center color-lightgray">{info.graduation.date}</td>
+                    <td className="p-12 text-center"><Link href={info.apply.link} className="fs-14 fw-700 lh-20 color-royalblue">{info.apply.text}</Link></td>
 
-                    <div className="p-12 fs-14 fw-600 lh-20">{info.course.title}</div>
-                    <div className="p-12">
-                      <span className="bg-light-gray rounded-16 p-4-12 fs-12 fw-600 lh-16 color-lightgray">{info.course.modality}</span>
-                    </div>
-                  </td>
-                  <td className="p-12 fs-14 fw-400 lh-20 text-center color-lightgray">{info.start_date.date}</td>
-                  <td className="p-12 fs-14 fw-400 lh-20 text-center color-lightgray">{info.graduation.date}</td>
-                  <td className="p-12 text-center"><Link href={info.apply.link} className="fs-14 fw-700 lh-20 color-royalblue">{info.apply.text}</Link></td>
-              
-                </>)
-              })}
-          </tr>
+                  </>)
+                })}
+            </tr>
+          </tbody>
         </table>
       </div>
 
--- app/courses/career/software-engineering/page.js
+++ app/courses/career/software-engineering/page.js
@@ -21,7 +21,7 @@ const SoftwareEngineering = () => {
 
       <UpcomingDates data={data?.upcoming_dates} />
 
-      <div className="bg-darkblue py-50 ">
+      {/* <div className="bg-darkblue py-50 ">
         <div className='row justify-beetwen maxwidth-1366 m-auto'>
 
           <div className='column '>
@@ -41,43 +41,46 @@ const SoftwareEngineering = () => {
             >{data?.band?.button.button_text}</div>
           </Link>
         </div>
-      </div>
+      </div> */}
 
       <TwoColumns left={data?.two_columns?.left} right={data?.two_columns?.right} />
 
-      <div className='w-70 maxwidth-1366 bg-lila rounded-8 p-32 row gap-45 m-auto'>
-        <div className='w-fit-content '>
-          <Image
-            className='rounded-8'
-            src={data?.message?.image}
-            width={272}
-            height={272}
-          />
-        </div>
-        <div className='column justify-center'>
+      <div className=' p-sm-20 container'>
+        <div className='w-70 w-100-tablet maxwidth-1366 bg-lila rounded-8 p-32 row gap-45 m-auto column-tablet'>
+          <div className='w-fit-content w-100-tablet'>
+            <Image
+              className='rounded-8 w-100-tablet h-fit-content'
+              src={data?.message?.image}
+              width={272}
+              height={272}
+            />
+          </div>
+          <div className='column justify-center'>
 
-          <p className='fs-24 fw-500 lh-32'><Icon icon="quote-open" />{" " + data?.message?.paragraph + " "}<Icon icon="quote-close" /></p>
-          <div className='row pt-32 fs-14 lh-20'>
-            <p className='fw-700'>{"  - " + data?.message?.name}</p><p>{", " + data?.message?.job}</p>
+            <p className='fs-24 fw-500 lh-32'><Icon icon="quote-open" />{" " + data?.message?.paragraph + " "}<Icon icon="quote-close" /></p>
+            <div className='row pt-32 fs-14 lh-20'>
+              <p className='fw-700'>{"  - " + data?.message?.name}</p><p>{", " + data?.message?.job}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='py-50 maxwidth-1366 m-auto'>
+          <div className='text-center py-30 w-100 m-auto column gap-8'>
+            <h4 className='fs-16 fw-600 lh-24 color-royalblue'>{data?.what_learn?.sub_title}</h4>
+            <h2 className='fs-30 fw-800 lh-36 fs-sm-24'>{data?.what_learn?.title}</h2>
+            <p className='fs-18 fw-400 lh-24 color-gray'>{data?.what_learn?.text}</p>
+          </div>
+          <div className='grid container grid-col-3 w-100 w-100-tablet gap-24 grid-col-sm-1 grid-col-tablet-2'>
+            {data?.what_learn?.items.map(item =>
+              <div className='row gap-8 color-gray'>
+                <div>
+                  <Icon icon={data?.what_learn?.icon} width="20px" />
+                </div>
+                <p className='fs-16 fw-500 lh-24'>{item}</p>
+              </div>)}
           </div>
         </div>
       </div>
-
-      <div className='py-50 maxwidth-1366 m-auto'>
-        <div className='text-center py-30 w-60 m-auto column gap-8'>
-          <h4 className='fs-16 fw-600 lh-24'>{data?.what_learn?.sub_title}</h4>
-          <h2 className='fs-30 fw-800 lh-36'>{data?.what_learn?.title}</h2>
-          <p className='fs-18 fw-400 lh-24 color-gray'>{data?.what_learn?.text}</p>
-        </div>
-        <div className='grid grid-col-3 w-60 m-auto gap-24'>
-          {data?.what_learn?.items.map(item =>
-            <div className='row gap-8 color-gray'>
-              <Icon icon={data?.what_learn?.icon} width="24px" />
-              <p>{item}</p>
-            </div>)}
-        </div>
-      </div>
-
       <TwoColumns left={data?.two_columns_2?.left} right={data?.two_columns_2?.right} />
 
       {/* <Testimonials/> */}
@@ -90,7 +93,7 @@ const SoftwareEngineering = () => {
       />
 
       {data?.qualities && (
-        <div className='row gap-45 wrap py-30 justify-center maxwidth-1366 m-auto justify-beetwen'>
+        <div className='container row gap-45 wrap py-30 justify-center maxwidth-1366 m-auto justify-beetwen'>
 
           {data?.qualities?.map((qualitie, index) => {
             return (
