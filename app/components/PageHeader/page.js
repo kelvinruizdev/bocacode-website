401659961056cef3742ab65ac44eb4650eb8ebdb
Kelvin <kelvinruiz.developer@gmail.com>
2024-01-10 14:07:21 -0400

    eliminar componentes vacios

--- app/components/PageHeader/page.js
+++ app/components/PageHeader/page.js
@@ -7,7 +7,7 @@ import TwoColumns from '../TwoColumns/page'
 
 const PageHeader = ({ children, backgroundImage, data }) => {
 
-    const { heading, sub_heading, content, button_1, button_2 } = data;
+    // const { heading, sub_heading, content, button_1, button_2 } = data;
 
     return (
         <>
@@ -21,7 +21,7 @@ const PageHeader = ({ children, backgroundImage, data }) => {
                 <div className="pageheader-container-layer">
                     <div className="flex column pageheader-content maxwidth-1366">
                         {
-                            heading &&
+                            data?.heading &&
                             <div className="w-50 w-100-tablet">
                                 <h1
                                     className="fs-48 fw-800 lh-48 fs-tablet-30 lh-tablet-36 color-white-v2"
@@ -36,7 +36,7 @@ const PageHeader = ({ children, backgroundImage, data }) => {
                             </div>
                         }
                         {
-                            sub_heading &&
+                            data?.sub_heading &&
                             <h2
                                 className=" w-50 w-100-tablet"
                                 //style={{ color: `${sub_heading.color}`, fontSize: `${sub_heading.font_size}`, fontWeight: `${sub_heading.font_weight}`, lineHeight: `${sub_heading.line_height}` }}
@@ -44,7 +44,7 @@ const PageHeader = ({ children, backgroundImage, data }) => {
                             />
                         }
                         {
-                            content &&
+                            data?.content &&
                             <p
                                 className="fs-20 fw-400 lh-28 fs-tablet-16 w-50 w-100-tablet color-white-v2"
                                 //style={{ color: `${content.color}`, fontSize: `${content.font_size}`, fontWeight: `${content.font_weight}`, lineHeight: `${content.line_height}` }}
@@ -52,27 +52,27 @@ const PageHeader = ({ children, backgroundImage, data }) => {
                             />
                         }
                         {
-                            button_1 && (
+                            data?.button_1 && (
                                 <div
                                     className="buttons-container text-center flex w-50 w-100-tablet" 
                                 >
-                                    <Link href={button_1.button_link}>
+                                    <Link href={data?.button_1.button_link}>
                                         <div
                                             style={{
-                                                background: `${button_1.button_color}`,
-                                                color: `${button_1.button_textcolor}`
+                                                background: `${data?.button_1.button_color}`,
+                                                color: `${data?.button_1.button_textcolor}`
                                             }}
                                             className="pageheader-button_1 rounded-4"
-                                        >{button_1.button_text}</div>
+                                        >{data?.button_1.button_text}</div>
                                     </Link>
-                                    {button_2 &&
-                                        <Link href={button_2.button_link}>
+                                    {data?.button_2 &&
+                                        <Link href={data?.button_2.button_link}>
                                             <div
                                                 style={{
-                                                    background: `${button_2.button_color}`
+                                                    background: `${data?.button_2.button_color}`
                                                 }}
                                                 className="pageheader-button_2 rounded-4"
-                                            >{button_2.button_text}</div>
+                                            >{data?.button_2.button_text}</div>
                                         </Link>}
 
                                 </div>
