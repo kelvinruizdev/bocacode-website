5031a2732cd7cf69d915f2e2ba56750927ea493e
Kelvin <kelvinruiz.developer@gmail.com>
2024-01-10 13:21:22 -0400

    cambio de funcion flecha a function

--- app/components/Icon/set/arrow.js
+++ app/components/Icon/set/arrow.js
@@ -1,17 +1,8 @@
 import React from "react";
-export default (props) => (
+export default function Arrow (props) {
+    return(
     <svg class={props.iconClass} fill="none" width="20px" height="20px" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M9 5l7 7-7 7" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
             </path>
-    </svg>
-);
-
-
-
-
-
-
-
-
-
-
+    </svg>)
+};
--- app/components/Icon/set/check-v2.js
+++ app/components/Icon/set/check-v2.js
@@ -1,7 +1,8 @@
 import React from "react";
-export default (props) => (
+export default function Checkv2 (props) {
+    return(
     <svg class="rounded-total" style={{background: "#DCFCE7", padding: "2px"}} width={props.width} height={props.width} viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-);
+)};
 
 
 
